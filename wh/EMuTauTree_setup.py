0cd52ee9c53976d24158dfc9f11433ed0cf12929
Evan Friis <ekfriis@gmail.com>
2012-09-12 08:16:23 -0500

    stupid path change

--- wh/EMuTauTree_setup.py
+++ wh/EMuTauTree_setup.py
@@ -7,8 +7,8 @@ from Cython.Distutils import build_ext
 setup(ext_modules=[Extension(
     "EMuTauTree",                 # name of extension
     ["EMuTauTree.pyx"], #  our Cython source
-    include_dirs=['/cvmfs/cms.hep.wisc.edu/osg/app/cmssoft/cms/slc5_amd64_gcc434/lcg/root/5.27.06b-cms23/include'],
-    library_dirs=['/cvmfs/cms.hep.wisc.edu/osg/app/cmssoft/cms/slc5_amd64_gcc434/lcg/root/5.27.06b-cms23/lib'],
+    include_dirs=['/cvmfs/cms.hep.wisc.edu/osg/app/cmssoft/cms/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_2_5/external/slc5_amd64_gcc462/bin/../../../../../../lcg/root/5.32.00-cms6/include'],
+    library_dirs=['/cvmfs/cms.hep.wisc.edu/osg/app/cmssoft/cms/slc5_amd64_gcc462/cms/cmssw/CMSSW_5_2_5/external/slc5_amd64_gcc462/bin/../../../../../../lcg/root/5.32.00-cms6/lib'],
     libraries=['Tree', 'Core', 'TreePlayer'],
     language="c++")],  # causes Cython to create C++ source
     cmdclass={'build_ext': build_ext})
