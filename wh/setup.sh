f008bf21065203cfb5367986240529d50e88ea5a
Evan Friis <ekfriis@gmail.com>
2012-10-23 09:56:32 -0500

    Factor out proxy builder script

--- wh/setup.sh
+++ wh/setup.sh
@@ -5,19 +5,8 @@
 source jobid.sh
 export datasrc=/scratch/efriis/data/
 export jobid=$jobid7
-export afile=`find $datasrc/$jobid | grep root | head -n 1`
 
-echo "Building cython wrappers from file: $afile"
-
-rake "make_wrapper[$afile, eet/final/Ntuple, EETauTree]"
-rake "make_wrapper[$afile, emt/final/Ntuple, EMuTauTree]"
-rake "make_wrapper[$afile, mmt/final/Ntuple, MuMuTauTree]"
-rake "make_wrapper[$afile, mmm/final/Ntuple, MuMuMuTree]"
-rake "make_wrapper[$afile, mm/final/Ntuple, MuMuTree]"
-rake "make_wrapper[$afile, em/final/Ntuple, EMuTree]"
-rake "make_wrapper[$afile, ee/final/Ntuple, EETree]"
-
-ls *pyx | sed "s|pyx|so|" | xargs rake 
+./make_proxies.sh
 
 rake "meta:getinputs[$jobid, $datasrc]"
 rake "meta:getmeta[inputs/$jobid, mm/metaInfo, 7]"
@@ -31,4 +20,3 @@ pushd inputs/$jobid/
 ls ../../inputs/$jobid7/WH_*HWW* | grep -v lumicalc | xargs -n 1 ln -s 
 popd
 rake "meta:getmeta[inputs/$jobid, mm/metaInfo, 8]"
-
