808f71845bbf6240dc3b69a286d9f85df4de85d2
Evan Friis <ekfriis@gmail.com>
2012-10-24 04:04:35 -0500

    Build in parallel

--- wh/make_proxies.sh
+++ wh/make_proxies.sh
@@ -5,7 +5,7 @@
 source jobid.sh
 
 export datasrc=/scratch/efriis/data/
-export jobid=$jobid7
+export jobid=$jobid8
 export afile=`find $datasrc/$jobid | grep root | head -n 1`
 
 echo "Building cython wrappers from file: $afile"
@@ -18,4 +18,4 @@ rake "make_wrapper[$afile, mm/final/Ntuple, MuMuTree]"
 rake "make_wrapper[$afile, em/final/Ntuple, EMuTree]"
 rake "make_wrapper[$afile, ee/final/Ntuple, EETree]"
 
-ls *pyx | sed "s|pyx|so|" | xargs rake 
+ls *pyx | sed "s|pyx|so|" | xargs -n 1 -P 10 rake 
