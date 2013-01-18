a8235edb588519eaabb2121b4784a4578cc878e6
Mauro Verzetti <mverzett@cern.ch>
2013-01-18 03:54:21 -0600

    several updates

--- wh/fakerate_functions.py
+++ wh/fakerate_functions.py
@@ -1,4 +1,11 @@
 import os
+from FinalStateAnalysis.StatTools.RooFunctorFromWS import build_roofunctor, make_corrector_from_th2
+import FinalStateAnalysis.MetaData.data_views as data_views
+import glob
+from TwoDimFakeRate import TwoDimFakeRate
+import fnmatch
+import logging
+data_views.log.setLevel(logging.INFO)
 
 ################################################################################
 #### Fitted fake rate functions ################################################
@@ -6,6 +13,12 @@ import os
 
 # Get fitted fake rate functions
 frfit_dir = os.path.join('results', os.environ['jobid'], 'fakerate_fits')
+
+
+##################
+## 1D Muon Func ##
+##################
+
 highpt_mu_fr = build_roofunctor(
     #frfit_dir + '/m_wjets_pt20_pfidiso02_muonJetPt.root',
     frfit_dir + '/m_wjets_pt20_h2taucuts_muonJetPt.root',
@@ -18,26 +31,44 @@ lowpt_mu_fr = build_roofunctor(
     'fit_efficiency', # workspace name
     'efficiency'
 )
-tau_fr = build_roofunctor(
-    frfit_dir + '/t_ztt_pt20_mvaloose_tauPt.root',
-    'fit_efficiency', # workspace name
-    'efficiency'
-)
 
 highpt_mu_qcd_fr = build_roofunctor(
     frfit_dir + '/m_qcd_pt20_h2taucuts_muonJetPt.root',
     'fit_efficiency', # workspace name
     'efficiency'
 )
+
 lowpt_mu_qcd_fr = build_roofunctor(
     frfit_dir + '/m_qcd_pt10_h2taucuts_muonJetPt.root',
     'fit_efficiency', # workspace name
     'efficiency'
 )
 
+#######################
+## 1D Electrons Func ##
+#######################
+
 lowpt_e_qcd_fr = build_roofunctor(
     #frfit_dir + '/e_qcd_pt10_mvaidiso03_eJetPt.root',
     frfit_dir + '/e_qcd_pt10_h2taucuts_eJetPt.root',
+    'fit_efficiency', # workspace name
+    'efficiency'
+)
+
+lowpt_e_fr = build_roofunctor(
+    #frfit_dir + '/e_wjets_pt10_mvaidiso03_eJetPt.root',
+    frfit_dir + '/e_wjets_pt10_h2taucuts_eJetPt.root',
+    'fit_efficiency', # workspace name
+    'efficiency'
+)
+
+
+##################
+## 1D Taus Func ##
+##################
+
+tau_fr = build_roofunctor(
+    frfit_dir + '/t_ztt_pt20_mvaloose_tauPt.root',
     'fit_efficiency', # workspace name
     'efficiency'
 )
@@ -47,6 +78,19 @@ tau_qcd_fr = tau_fr ## build_roofunctor(
 ##     'fit_efficiency', # workspace name
 ##     'efficiency'
 ## )
+
+highpt_ee_fr = build_roofunctor(
+    frfit_dir + '/ee_wjets_pt20_mvaidiso01_e2JetPt-data_ee.root',
+    'fit_efficiency', # workspace name
+    'efficiency'
+)
+
+lowpt_ee_fr = build_roofunctor(
+    frfit_dir + '/ee_wjets_pt10_mvaidiso01_e2JetPt-data_ee.root',
+    'fit_efficiency', # workspace name
+    'efficiency'
+)
+
 
 # Get 2D fake rates
 
@@ -93,10 +137,8 @@ mu_fr_qcd_2d_b = TwoDimFakeRate(
     'qcd/pt10b/h2taucuts/muonJetVsLeptonPt', 'qcd/pt10b/muonJetVsLeptonPt',
     get_view('data'), get_view('WZ*'), get_view('ZZ*'))
 
-lowpt_e_fr = build_roofunctor(
-    #frfit_dir + '/e_wjets_pt10_mvaidiso03_eJetPt.root',
-    frfit_dir + '/e_wjets_pt10_h2taucuts_eJetPt.root',
-    'fit_efficiency', # workspace name
-    'efficiency'
-)
+
+e_charge_flip      = make_corrector_from_th2(frfit_dir+"/chare_flip_prob_map.root", "efficiency_map")         
+e_charge_flip_up   = make_corrector_from_th2(frfit_dir+"/chare_flip_prob_map.root", "efficiency_map_statUp")  
+e_charge_flip_down = make_corrector_from_th2(frfit_dir+"/chare_flip_prob_map.root", "efficiency_map_statDown")
 
