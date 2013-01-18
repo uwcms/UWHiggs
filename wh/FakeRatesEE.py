533ded7e6c6671043609bd989c64513a4fdbe6e9
Mauro Verzetti <mverzett@cern.ch>
2013-01-18 03:48:57 -0600

    added more histograms

--- wh/FakeRatesEE.py
+++ wh/FakeRatesEE.py
@@ -25,6 +25,7 @@ import baseSelections as selections
 import os
 import ROOT
 import array
+from pprint import pprint
 
 math = ROOT.TMath
 def make_corrector_from_th2(filename, path):
@@ -80,20 +81,6 @@ def sc_inv_mass(row):
     pt2 = row.e2SCEnergy / math.CosH(row.e2SCEta)
     return math.Sqrt(2*pt1*pt2*( math.CosH(row.e1SCEta - row.e2SCEta) - math.Cos(row.e1SCPhi - row.e2SCPhi) ) )
 
-def control_region(row):
-    # Figure out what control region we are in.
-    if row.e1_e2_SS and row.e1RelPFIsoDB < 0.15 and row.e1MtToMET > 50 and row.metSignificance > 3:
-        return 'wjets'
-    elif row.e1_e2_SS and row.e1RelPFIsoDB > 0.3 and row.metSignificance < 3:
-        return 'qcd'
-    elif row.e1RelPFIsoDB < 0.1 and row.e2RelPFIsoDB < 0.1 and row.e2MVAIDH2TauWP and row.e1MVAIDH2TauWP \
-        and not any([ row.muVetoPt5,
-                      row.tauVetoPt20,
-                      row.eVetoCicTightIso,
-                      ]):
-        return 'zee'
-    else:
-        return None
  
 class FakeRatesEE(MegaBase):
     tree = 'ee/final/Ntuple'
@@ -107,7 +94,7 @@ class FakeRatesEE(MegaBase):
         self.is7TeV = '7TeV' in os.environ['jobid']
 
     def begin(self):
-        for region in ['wjets', 'qcd']:
+        for region in ['wjets', 'qcd', 'wjetsNoZmass']:
             for denom in ['pt10', 'pt20']:
                 denom_key = (region, denom)
                 denom_histos = {}
@@ -131,7 +118,17 @@ class FakeRatesEE(MegaBase):
                     book_histo('e2JetPt', 'e Jet Pt', 100, 0, 100)
                     book_histo('e2AbsEta', 'e Abs Eta', 100, -2.5, 2.5)
                     book_histo('metSignificance', 'MET sig.', 100, 0, 10)
-                    book_histo('e1MtToMET', 'm MT', 100, 0, 200)
+                    book_histo('e1MtToMET', 'e1 MT', 100, 0, 200)
+                    book_histo('e2MtToMET', 'e2 MT', 100, 0, 200)
+                    book_histo('e1MtToMET_Z', 'e1 MT', 100, 0, 200)
+                    book_histo('e2MtToMET_Z', 'e2 MT', 100, 0, 200)
+                    book_histo('e1MtToMET_NoZ', 'e1 MT', 100, 0, 200)
+                    book_histo('e2MtToMET_NoZ', 'e2 MT', 100, 0, 200)
+                    book_histo('MET', 'MET', 100, 0, 400)
+                    book_histo('MET_Z', 'MET, Z Events', 100, 0, 400)
+                    book_histo('MET_NoZ', 'MET, No Z Events', 100, 0, 400)
+                    book_histo('OSS', 'SS to OS', 2, 0, 2)
+                    book_histo('OSS_Z', 'SS to OS, Z events', 2, 0, 2)
                     book_histo('e1e2Mass', 'DiElectron Mass', 100, 0, 200)
                     book_histo('doubleEPrescale', 'prescale', 10, -0.5, 9.5)
         # Charge mis-ID measurements
@@ -149,6 +146,29 @@ class FakeRatesEE(MegaBase):
         self.book('charge', "SS"+'_SCDPhi', 'electron Super Cluster Delta phi', 180, 0, math.Pi())
         self.book('charge', 'SS_ePt', 'e Pt', 100, 0, 100)
         self.book('charge', 'SS_eAbsEta', 'e Abs Eta', 80, 0, 2.5)
+
+        num_key = ('charge','checks')
+        num_histos = {}
+        self.histograms[num_key] = num_histos
+
+        num_histos['e2Pt'           ] = self.book('charge/checks', 'e2Pt'           , 'e Pt', 100, 0, 100)
+        num_histos['e2JetPt'        ] = self.book('charge/checks', 'e2JetPt'        , 'e Jet Pt', 100, 0, 100)
+        num_histos['e2AbsEta'       ] = self.book('charge/checks', 'e2AbsEta'       , 'e Abs Eta', 100, -2.5, 2.5)
+        num_histos['metSignificance'] = self.book('charge/checks', 'metSignificance', 'MET sig.', 100, 0, 10)
+        num_histos['e1MtToMET'      ] = self.book('charge/checks', 'e1MtToMET'      , 'e1 MT', 100, 0, 200)
+        num_histos['e2MtToMET'      ] = self.book('charge/checks', 'e2MtToMET'      , 'e2 MT', 100, 0, 200)
+        num_histos['e1MtToMET_Z'    ] = self.book('charge/checks', 'e1MtToMET_Z'    , 'e1 MT', 100, 0, 200)
+        num_histos['e2MtToMET_Z'    ] = self.book('charge/checks', 'e2MtToMET_Z'    , 'e2 MT', 100, 0, 200)
+        num_histos['e1MtToMET_NoZ'  ] = self.book('charge/checks', 'e1MtToMET_NoZ'  , 'e1 MT', 100, 0, 200)
+        num_histos['e2MtToMET_NoZ'  ] = self.book('charge/checks', 'e2MtToMET_NoZ'  , 'e2 MT', 100, 0, 200)
+        num_histos['MET'            ] = self.book('charge/checks', 'MET'            , 'MET', 100, 0, 400)
+        num_histos['MET_Z'          ] = self.book('charge/checks', 'MET_Z'          , 'MET, Z Events', 100, 0, 400)
+        num_histos['MET_NoZ'        ] = self.book('charge/checks', 'MET_NoZ'        , 'MET, No Z Events', 100, 0, 400)
+        num_histos['OSS'            ] = self.book('charge/checks', 'OSS'            , 'SS to OS', 2, 0, 2)
+        num_histos['OSS_Z'          ] = self.book('charge/checks', 'OSS_Z'          , 'SS to OS, Z events', 2, 0, 2)
+        num_histos['e1e2Mass'       ] = self.book('charge/checks', 'e1e2Mass'       , 'DiElectron Mass', 100, 0, 200)
+        num_histos['doubleEPrescale'] = self.book('charge/checks', 'doubleEPrescale', 'prescale', 10, -0.5, 9.5)
+
             
         #print self.histograms.keys()
 
@@ -169,15 +189,29 @@ class FakeRatesEE(MegaBase):
             the_histos['e2AbsEta'].Fill(row.e2AbsEta)
             the_histos['metSignificance'].Fill(row.metSignificance)
             the_histos['e1MtToMET'].Fill(row.e1MtToMET)
+            the_histos['e2MtToMET'].Fill(row.e2MtToMET)
+            the_histos['MET'].Fill(row.metEt)
+            the_histos['OSS'].Fill(int(row.e1_e2_SS))
+            if row.e1_e2_Mass > 60 and row.e1_e2_Mass < 120 :
+                the_histos['MET_Z'].Fill(row.metEt)
+                the_histos['OSS_Z'].Fill(int(row.e1_e2_SS))
+                the_histos['e1MtToMET_Z'].Fill(row.e1MtToMET)
+                the_histos['e2MtToMET_Z'].Fill(row.e2MtToMET)
+            else:
+                the_histos['MET_NoZ'].Fill(row.metEt)
+                the_histos['e1MtToMET_NoZ'].Fill(row.e1MtToMET)
+                the_histos['e2MtToMET_NoZ'].Fill(row.e2MtToMET)
+                
             the_histos['e1e2Mass'].Fill(row.e1_e2_Mass)
             the_histos['doubleEPrescale'].Fill(row.doubleEPrescale)
 
         histos = self.histograms
+        #pprint( histos)
         for row in self.tree:
             if not preselection(row):
                 continue
 
-            region = control_region(row)
+            region = selections.control_region_ee(row)
             if region is None:
                 continue
 
@@ -205,42 +239,32 @@ class FakeRatesEE(MegaBase):
                     histos['charge/OS'+app+'_SCDPhi'].Fill(delta_phi(row.e1SCPhi, row.e2SCPhi), weight )
                     histos['charge/OS'+app+'_TrkMass'].Fill(row.e1_e2_Mass, weight)
                     histos['charge/OS'+app+'_SCMass'].Fill(scmass, weight)
+                fill(histos[('charge','checks')], row)
                 continue
             # This is a QCD or Wjets
-            fill(histos[(region, 'pt10')], row)
 
-            if row.e2MVAIDH2TauWP:
-                fill(histos[(region, 'pt10', 'mvaid')], row)
+            def make_region_plots(full_region):
+                fill(histos[full_region], row)
+                if row.e2MVAIDH2TauWP:
+                    fill(histos[full_region + ( 'mvaid',)], row)
+                if row.e2RelPFIsoDB < 0.3:
+                    fill(histos[full_region + ( 'iso03',)], row)
+                if row.e2MVAIDH2TauWP and row.e2RelPFIsoDB < 0.3:
+                    fill(histos[full_region + ( 'mvaidiso03',)], row)
+                if row.e2MVAIDH2TauWP and row.e2RelPFIsoDB < 0.1:
+                    fill(histos[full_region + ( 'mvaidiso01',)], row)
+                if row.e2MVAIDH2TauWP and ((row.e2RelPFIsoDB < 0.15 and row.e2AbsEta < 1.479) or row.e2RelPFIsoDB < 0.1):
+                    fill(histos[full_region + ( 'h2taucuts',)], row)
 
-            if row.e2RelPFIsoDB < 0.3:
-                fill(histos[(region, 'pt10', 'iso03')], row)
-
-            if row.e2MVAIDH2TauWP and row.e2RelPFIsoDB < 0.3:
-                fill(histos[(region, 'pt10', 'mvaidiso03')], row)
-
-            if row.e2MVAIDH2TauWP and row.e2RelPFIsoDB < 0.1:
-                fill(histos[(region, 'pt10', 'mvaidiso01')], row)
-
-            if row.e2MVAIDH2TauWP and ((row.e2RelPFIsoDB < 0.15 and row.e2AbsEta < 1.479) or row.e2RelPFIsoDB < 0.1):
-                fill(histos[(region, 'pt10', 'h2taucuts')], row)
+            make_region_plots((region, 'pt10'))
+            if region == 'wjets' and not (row.e1_e2_Mass > 70 and row.e1_e2_Mass < 110):
+                make_region_plots(('wjetsNoZmass', 'pt10'))
 
             if row.e2Pt > 20:
-                fill(histos[(region, 'pt20')], row)
-                if row.e2MVAIDH2TauWP:
-                    fill(histos[(region, 'pt20', 'mvaid')], row)
-
-                if row.e2RelPFIsoDB < 0.3:
-                    fill(histos[(region, 'pt20', 'iso03')], row)
-
-                if row.e2MVAIDH2TauWP and row.e2RelPFIsoDB < 0.3:
-                    fill(histos[(region, 'pt20', 'mvaidiso03')], row)
-
-                if row.e2MVAIDH2TauWP and row.e2RelPFIsoDB < 0.1:
-                    fill(histos[(region, 'pt20', 'mvaidiso01')], row)
-
-                if row.e2MVAIDH2TauWP and ((row.e2RelPFIsoDB < 0.15 and row.e2AbsEta < 1.479) or row.e2RelPFIsoDB < 0.1):
-                    fill(histos[(region, 'pt10', 'h2taucuts')], row)
-                
+                make_region_plots((region, 'pt20'))
+                if region == 'wjets' and not (row.e1_e2_Mass > 70 and row.e1_e2_Mass < 110):
+                    make_region_plots(('wjetsNoZmass', 'pt20'))
+            
 
     def finish(self):
         self.write_histos()
