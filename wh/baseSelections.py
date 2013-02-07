432b4cc16ac40654a1f34d2d9c862d4379028ded
Mauro Verzetti <mverzett@cern.ch>
2013-02-07 04:13:27 -0600

    added h2tau electron id function

--- wh/baseSelections.py
+++ wh/baseSelections.py
@@ -35,13 +35,19 @@ def vetos(row):
     if row.eVetoCicTightIso:  return False
     return True
 
+def h2tau_eid(row, name):
+    MVAIDH2TauWP = getattr(row, getVar(name, 'MVAIDH2TauWP'))
+    RelPFIsoDB   = getattr(row, getVar(name, 'RelPFIsoDB'))
+    AbsEta       = getattr(row, getVar(name, 'AbsEta'))
+    return bool(MVAIDH2TauWP) and bool( RelPFIsoDB < 0.1 or (RelPFIsoDB < 0.15 and AbsEta < 1.479))
+
 def control_region_ee(row):
     '''Figure out what control region we are in. Shared among two codes, to avoid mismatching copied here'''
     if  row.e1_e2_SS and row.e1RelPFIsoDB < 0.15 and row.e1MtToMET > 30 and row.e2MtToMET < 30:# and row.e2MtToMET < 30: #and row.metEt > 30: #row.metSignificance > 3:
         return 'wjets'
     elif row.e1_e2_SS and row.e1RelPFIsoDB > 0.3 and row.metEt < 25: #and row.metSignificance < 3: #
         return 'qcd'
-    elif row.e1RelPFIsoDB < 0.1 and row.e2RelPFIsoDB < 0.1 and row.e2MVAIDH2TauWP and row.e1MVAIDH2TauWP \
+    elif h2tau_eid(row,'e1') and h2tau_eid(row,'e2') \
         and not any([ row.muVetoPt5,
                       row.tauVetoPt20,
                       row.eVetoCicTightIso,
