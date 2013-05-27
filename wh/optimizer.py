5a8adce63ac746b73f13bf721f26a730fe874d9b
Mauro Verzetti <mverzett@cern.ch>
2013-05-27 09:13:10 -0500

    added new electron MVA discriminator

--- wh/optimizer.py
+++ wh/optimizer.py
@@ -8,14 +8,20 @@ RUN_OPTIMIZATION = 'RUN_OPTIMIZATION' in os.environ and bool(os.environ['RUN_OPT
 lep_id = [
     'h2taucuts',
     'h2taucuts020',
-    'idiso02'
+    'idiso02',
+    'eid13h2taucuts',
+    'eid13h2taucuts020',
+    'eid13idiso02',
     ] if RUN_OPTIMIZATION else ['h2taucuts', 'h2taucuts020']
 
-LT_cut = [60, 70, 80, 90]
+LT_cut = [0, 60, 80, 100, 120, 140]
 
 grid_search = {}
 if RUN_OPTIMIZATION:
     for cut_combo in itertools.product(lep_id, lep_id, LT_cut):
+        if (cut_combo[0].startswith('eid13') or cut_combo[1].startswith('eid13')) and \
+            not (cut_combo[0].startswith('eid13') and cut_combo[1].startswith('eid13')):
+            continue
         cut_name = '%s:%s:%s' % cut_combo
         grid_search[cut_name] = {
             'leading_iso' : cut_combo[0],
