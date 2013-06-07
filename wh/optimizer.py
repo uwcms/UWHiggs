8c91e007b8987fe389954c61fc39de6e55f15d2b
Mauro Verzetti <mverzett@cern.ch>
2013-06-07 03:49:37 -0500

    added new set of working points

--- wh/optimizer.py
+++ wh/optimizer.py
@@ -6,34 +6,58 @@ import itertools
 RUN_OPTIMIZATION = 'RUN_OPTIMIZATION' in os.environ and bool(os.environ['RUN_OPTIMIZATION'])
 
 lep_id = [
-    'h2taucuts',
-    'h2taucuts020',
-    'idiso02',
-    'eid13h2taucuts',
-    'eid13h2taucuts020',
-    'eid13idiso02',
+    'eid13Looseh2taucuts',
+    'eid13Looseh2taucuts020',
+    'eid13Looseidiso02',
+    'eid13Tighth2taucuts',
+    'eid13Tighth2taucuts020',
+    'eid13Tightidiso02',
     ] if RUN_OPTIMIZATION else ['h2taucuts', 'h2taucuts020']
 
-LT_cut = [0, 60, 80, 100, 120, 140]
+LT_cut = [0, 80, 110, 140, 170, 200, 3000]
+
+tauIDs = ['tMediumIso3Hits',None]
+
+tauPTs = [0, 30, 40, 60]
+
+charge_fakes = [70, 80, 90]
 
 grid_search = {}
+counter_1, counter_2, counter_3, counter_4 = 0, 0, 0, 0
 if RUN_OPTIMIZATION:
-    for cut_combo in itertools.product(lep_id, lep_id, LT_cut):
-        if (cut_combo[0].startswith('eid13') or cut_combo[1].startswith('eid13')) and \
-            not (cut_combo[0].startswith('eid13') and cut_combo[1].startswith('eid13')):
+    for lead_id, sublead_id, lt_thr, tauID, tauPT, charge_fakes in \
+        itertools.product(lep_id, lep_id, LT_cut, tauIDs, tauPTs, charge_fakes):
+        cut_name = '%s:%s:%s:%s:%s:%s' % (lead_id, sublead_id, lt_thr, tauID, tauPT, charge_fakes)
+        counter_1 += 1
+        if (lead_id.startswith('eid13Loose') or sublead_id.startswith('eid13Loose')) and \
+            not (lead_id.startswith('eid13Loose') and sublead_id.startswith('eid13Loose')):
             continue
-        cut_name = '%s:%s:%s' % cut_combo
+        counter_2 += 1 
+        if tauPT > 0 and lt_thr > 0:
+            continue
+        counter_3 += 1
+        if lt_thr and not tauID:
+            continue
+        counter_4 += 1
         grid_search[cut_name] = {
-            'leading_iso' : cut_combo[0],
-            'subleading_iso' : cut_combo[1],
-            'LT'   : cut_combo[2],
+            'leading_iso' : lead_id,
+            'subleading_iso' : sublead_id,
+            'LT'   : lt_thr,
+            'tauID': tauID,
+            'tauPT': tauPT,
+            'charge_fakes' : charge_fakes,
         }
 else:
     grid_search[''] = {
         'leading_iso'    : 'h2taucuts',
         'subleading_iso' : 'h2taucuts',
         'LT'             : 80,
+        'tauID': None,
+        'tauPT': 0,
+        'charge_fakes' : 80,
     }
+
+    #print counter_1, counter_2, counter_3, counter_4
 
 if __name__ == "__main__":
     print '\n'.join(grid_search.keys())
