4887d56d22d7a6d52b0c1b82ca39d86689245e3c
Aaron Levine <aglevine@login06.hep.wisc.edu>
2013-07-08 05:53:58 -0500

    fixed scaling bug in qcd creation

--- lfvmutau/plot_mutau.py
+++ lfvmutau/plot_mutau.py
@@ -5,9 +5,9 @@ import sys
 
 ##get qcd (choose selections for qcd)
 
-def make_qcd_norm(presel, var, predir, savedir, channel ,wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file,data_ntuple_file):
+def make_qcd_norm(presel, var, predir, savedir, channel ,wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file,data_ntuple_file,wjets_norm,zjets_norm,ttbar_norm,ww_norm):
 
-        qcd_norm_histo = get_ss_inc_qcd(var,channel, wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file)  ##gets same sign inclusive qcd
+        qcd_norm_histo = get_ss_inc_qcd(var,channel, wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file,wjets_norm,zjets_norm,ttbar_norm,ww_norm)  ##gets same sign inclusive qcd
         qcd_norm_histo.Scale(1.06) ##os inclusive qcd 
 	if not presel: #get efficiency of vbf cuts
      		ssanti_iso_ntuple_spot = "ssantiisomuon"+channel
@@ -22,44 +22,52 @@ def make_qcd_norm(presel, var, predir, savedir, channel ,wjets_pre_ntuple_file, 
 
 
 
-def get_ss_inc_qcd(var,channel, wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file):
+def get_ss_inc_qcd(var,channel, wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file,wjets_norm,zjets_norm,ttbar_norm,ww_norm):
 
 	ss_ntuple_spot = "ss"+channel
 	zjets_pre_ntuple_file.cd(ss_ntuple_spot)
 	zjets_pre = ROOT.gDirectory.Get(var).Clone()
+	zjets.Scale(zjets_norm)
 	ttbar_pre_ntuple_file.cd(ss_ntuple_spot)
 	ttbar_pre = ROOT.gDirectory.Get(var).Clone()
+	ttbar.Scale(ttbar_norm)
 	ww_pre_ntuple_file.cd(ss_ntuple_spot)
 	ww_pre = ROOT.gDirectory.Get(var).Clone()
+	ww_pre.Scale(ww_norm)
 	data_pre_ntuple_file.cd(ss_ntuple_spot)
 	qcd_ss_inc = ROOT.gDirectory.Get(var).Clone()
 	qcd_ss_inc.Add(zjets_pre,-1)
 	qcd_ss_inc.Add(ttbar_pre,-1)
 	qcd_ss_inc.Add(ww_pre,-1)
-	wjets_pre = get_w(var,ss_ntuple_spot,wjets_pre_ntuple_file,zjets_pre_ntuple_file,ttbar_pre_ntuple_file,ww_pre_ntuple_file,data_pre_ntuple_file) #returns w+jets estimation
+	wjets_pre = get_w(var,ss_ntuple_spot,wjets_pre_ntuple_file,zjets_pre_ntuple_file,ttbar_pre_ntuple_file,ww_pre_ntuple_file,data_pre_ntuple_file, wjets_norm, zjets_norm, ttbar_norm, ww_norm) #returns w+jets estimation
 	qcd_ss_inc.Add(wjets_pre,-1)
 	return qcd_ss_inc
 
 	
 
-def get_w(var,ss_ntuple_spot, wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file):
+def get_w(var,ss_ntuple_spot, wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file,wjets_norm,zjets_norm,ttbar_norm,ww_norm):
 
 	ss_highmt_ntuple_spot = "highMt"+ss_ntuple_spot
 	data_pre_ntuple_file.cd(ss_highmt_ntuple_spot)
 	wjets_ss_inc = ROOT.gDirectory.Get(var).Clone() #data_ss_highmt
 	zjets_pre_ntuple_file.cd(ss_highmt_ntuple_spot)
         zjets_ss_highmt = ROOT.gDirectory.Get(var).Clone()
+	zjets_ss_highmt.Scale(zjets_norm)
         ttbar_pre_ntuple_file.cd(ss_highmt_ntuple_spot)
         ttbar_ss_highmt = ROOT.gDirectory.Get(var).Clone()
+	ttbar_ss_highmt.Scale(ttbar_norm)
         ww_pre_ntuple_file.cd(ss_highmt_ntuple_spot)
         ww_ss_highmt = ROOT.gDirectory.Get(var).Clone()
+	ww_ss_highmt.Scale(ww_norm)
 	wjets_pre_ntuple_file.cd(ss_highmt_ntuple_spot)
 	wjets_mc_ss_highmt = ROOT.gDirectory.Get(var).Clone()
+	wjets_mc_ss_highmt.Scale(wjets_norm)
 	wjets_pre_ntuple_file.cd(ss_ntuple_spot)
 	wjets_mc_ss = ROOT.gDirectory.Get(var).Clone()
+	wjets_mc_ss.Scale(wjets_norm)
 	wjets_ss_inc.Add(zjets_ss_highmt,-1)
 	wjets_ss_inc.Add(ttbar_ss_highmt,-1)
-	wjets_ss_inc.Add(ww_ss_highmt,-1) ##w_data_ss_highmt_
+	wjets_ss_inc.Add(ww_ss_highmt,-1) ##w_data_ss_highmt
 	wjets_ss_inc.Multiply(wjets_mc_ss)
 	wjets_ss_inc.Divide(wjets_mc_ss_highmt)
 	return wjets_ss_inc 
@@ -78,61 +86,84 @@ if shape_norm == False:
 else:
 	ynormlabel = "Normalized to 1 "
 binwidth = 5  #define rebinning
-
+blindlow = -10000
+blindhigh = 10000 ###initial low and high blinding to out of range variables, customize for individual variables
 #specify plotting schemes for individual variables
 if var == "mPt":
 	xlabel = "#mu P_{T} (GeV)"
 	binwidth = 10
 	legend =ROOT.TLegend(0.55,0.6,0.8,0.8,'','brNDC')
 	ylabel = ynormlabel +" 10 GeV binning"
+	blindlow = 40
+	blindhigh = 60
 elif var == "tPt":
 	xlabel = "#tau P_{T} (GeV)"
 	legend =ROOT.TLegend(0.45,0.6,0.8,0.8,'','brNDC')
 	ylabel = ynormlabel + " 5 GeV binning"
+	blindlow = 20
+	blindhigh = 30
 elif var == "mEta":
 	xlabel = "#mu #eta"
 	legend =ROOT.TLegend(0.2,0.6,0.4,0.8,'','brNDC')
 	binwidth = 10
 	ylabel = ynormlabel
+	blindlow = -1.5
+	blindhigh = 1.5
 elif var == "tEta":
 	xlabel = "#tau #eta"
 	legend =ROOT.TLegend(0.7,0.65,0.9,0.88,'','brNDC')
 	binwidth = 10
 	ylabel = ynormlabel
+	blindlow = -1.5
+	blindhigh = 1.5
 elif var == "mMtToPfMet_Ty1":
 	xlabel = "#mu M_{T} Ty1 (GeV)"
 	binwidth = 10
 	ylabel = ynormlabel + " 10 GeV binning"
 	legend =ROOT.TLegend(0.55,0.6,0.8,0.8,'','brNDC')
+	blindlow = 40
+	blindhigh = 100
 elif var == "tMtToPfMet_Ty1":
 	xlabel = "#tau M_{T} Ty1 (GeV)"
 	legend =ROOT.TLegend(0.45,0.6,0.8,0.8,'','brNDC')
 	ylabel = ynormlabel + " 5 GeV binning"
+	blindlow = 0
+	blindhigh = 20
 elif var == "vbfDeta":
 	xlabel = "#Delta#eta_{jj}"
 	legend =ROOT.TLegend(0.6,0.6,0.9,0.8,'','brNDC')
 	ylabel = ynormlabel
 	binwidth = 10
+	blindlow = 3.5
+	blindhigh = 6
 elif var == "vbfDijetrap":
 	xlabel ="Rapidity_{jj}"
 	legend =ROOT.TLegend(0.45,0.6,0.8,0.8,'','brNDC')
 	ylabel = ynormlabel
 	binwidth = 10
+	blindlow = 0
+	blindhigh = 1
 elif var == "vbfDphihjnomet":
 	xlabel = "#Delta#phi_{Hj} (no MET)"
 	legend =ROOT.TLegend(0.2,0.6,0.5,0.8,'','brNDC')
 	ylabel = ynormlabel
 	binwidth = 10
+	blindlow = 2.75
+	blindhigh = 3.25
 elif var == "vbfDphihj":
 	xlabel = "#Delta#phi_{Hj}"
 	legend =ROOT.TLegend(0.2,0.6,0.5,0.8,'','brNDC')
 	ylabel = ynormlabel
 	binwidth = 10
+	blindlow = 2.75
+	blindhigh = 3.25
 elif var == "vbfHrap":
 	xlabel = "Rapidity_{H}"
 	legend =ROOT.TLegend(0.4,0.6,0.89,0.8,'','brNDC')
 	ylabel = ynormlabel
 	binwidth = 10
+	blindlow = 0
+	blindhigh = 1
 elif var == "vbfj1eta":
 	xlabel = "#eta_{j1}"
 	binwidth = 10
@@ -148,6 +179,8 @@ elif var == "vbfMass":
 	binwidth = 10
 	legend =ROOT.TLegend(0.15,0.6,0.45,0.8,'','brNDC')
 	ylabel = ynormlabel + " 50 GeV binning"
+	blindlow = 500
+	blindhigh = 800
 elif var == "vbfMVA":
 	xlabel = "MVAMET (GeV)"
 	ylabel = ynormlabel
@@ -156,44 +189,59 @@ elif var == "vbfVispt":
 	xlabel = "Visible P_{T} (GeV)"
 	legend =ROOT.TLegend(0.45,0.6,0.8,0.8,'','brNDC')
 	ylabel = ynormlabel + " 10 GeV binning"
+	blindlow = 20
+	blindhigh = 80
 elif var == "m_t_Mass":
 	xlabel = "Visible Mass (GeV)"
 	legend =ROOT.TLegend(0.48,0.6,0.8,0.8,'','brNDC')
 	ylabel = ynormlabel + "10 GeV binning"
 	binwidth = 10
+	blindlow = 70
+	blindhigh = 120
 elif var == "m_t_DPhi":
 	xlabel = "#mu #tau #Delta#phi"
 	legend = ROOT.TLegend(0.25,0.6,0.6,0.8,'','brNDC')
 	ylabel = ynormlabel
+	blindlow = 2.6
+	blindhigh = 3.2
 elif var == "m_t_Pt":
 	xlabel = "Visible P_{T} (GeV)"
 	legend = ROOT.TLegend(0.45,0.5,0.85,0.8,'','brNDC')
 	ylabel = ynormlabel + "10 GeV binning"
 	binwidth = 10
+	blindlow = 20
+	blindhigh = 80
 elif var == "m_t_DR":
 	xlabel = "#mu #tau #DeltaR"
 	legend = ROOT.TLegend(0.45,0.6,0.8,0.8,'','brNDC')
 	ylabel = ynormlabel
+	blindlow = 2.5
+	blindhigh = 3.3
 elif var == "m_t_ToMETDPhi_Ty1":
 	xlabel = "#Delta#phi (MET , #mu#tau)"
 	legend = ROOT.TLegend(0.15,0.7,0.4,0.87,'','brNDC')
 	ylabel = ynormlabel
 	binwidth = 10
+	blindlow = 0
+	blindhigh = 1.6
 else:
 	xlabel = var
 	binwidth = 5
 	legend =ROOT.TLegend(0.45,0.6,0.8,0.8,'','brNDC')
 	ylabel = ynormlabel
+	
 
 predir = "presel_highmt/" #directory with preselection files
 channel = "vbf" #channel (vbf or gg)
 presel = False #use preselection cuts or not
-
+blind = True 
 ##savedir contains the root files for plotting
 if presel:
-	savedir = predir 
+	savedir = predir
+	
 else:
 	savedir = "vbf_highmt/"
+	
 
 canvas = ROOT.TCanvas("canvas","canvas",800,800)
 LFVStack = ROOT.THStack("stack","")
@@ -201,7 +249,10 @@ hgglfv_ntuple_file_str = 'LFV_GluGlu_H2Tau_M-126.root'
 hggsm_ntuple_file = ''
 hvbflfv_ntuple_file_str = 'LFV_VBF_H2Tau_M-126.root'
 zjets_ntuple_file_str = 'Zjets_M50.root'
+#wjets_ntuple_file_str = 'WJets.root'
 wjets_ntuple_file_str = 'WplusJets_madgraph_Extension.root'
+#wjets_ntuple_file_str = 'WplusJets_madgraph_filtered.root'
+wjets1_ntuple_file_str = 'Wplus1Jets_madgraph.root'
 ttbar_ntuple_file_str = 'TTplusJets_madgraph.root'
 ww_ntuple_file_str = 'WWJetsTo2L2Nu_TuneZ2_8TeV.root'
 data_ntuple_file_str = 'data_2012.root'
@@ -211,6 +262,7 @@ hgglfv_ntuple_file = ROOT.TFile(savedir+hgglfv_ntuple_file_str)
 hvbflfv_ntuple_file = ROOT.TFile(savedir+hvbflfv_ntuple_file_str)
 zjets_ntuple_file = ROOT.TFile(savedir+zjets_ntuple_file_str)
 wjets_ntuple_file = ROOT.TFile(savedir+wjets_ntuple_file_str)
+wjets1_ntuple_file = ROOT.TFile(savedir+wjets1_ntuple_file_str)
 ttbar_ntuple_file = ROOT.TFile(savedir+ttbar_ntuple_file_str)
 ww_ntuple_file = ROOT.TFile(savedir+ww_ntuple_file_str)
 data_ntuple_file = ROOT.TFile(savedir+data_ntuple_file_str)
@@ -220,18 +272,14 @@ hgglfv_pre_ntuple_file = ROOT.TFile(predir+hgglfv_ntuple_file_str)
 hvbflfv_pre_ntuple_file = ROOT.TFile(predir+hvbflfv_ntuple_file_str)
 zjets_pre_ntuple_file = ROOT.TFile(predir+zjets_ntuple_file_str)
 wjets_pre_ntuple_file = ROOT.TFile(predir+wjets_ntuple_file_str)
+wjets1_pre_ntuple_file = ROOT.TFile(predir+wjets1_ntuple_file_str)
 ttbar_pre_ntuple_file = ROOT.TFile(predir+ttbar_ntuple_file_str)
 ww_pre_ntuple_file = ROOT.TFile(predir+ww_ntuple_file_str)
 data_pre_ntuple_file = ROOT.TFile(predir+data_ntuple_file_str)
 
 
 
-qcd_norm_histo = make_qcd_norm(presel, var, predir, savedir, channel ,wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file,data_ntuple_file)
-antiiso_ntuple_spot = "antiisomuon"+channel
-data_ntuple_file.cd(antiiso_ntuple_spot)
-qcd = ROOT.gDirectory.Get(var).Clone() #get antiisolated qcd shape
 
-qcd.Scale(qcd_norm_histo.Integral()/qcd.Integral()) #normalized qcd shape
 
 #get histograms
 ntuple_spot = channel
@@ -261,7 +309,12 @@ data3_lumifile = 'lumicalc/data_SingleMu_Run2012B_22Jan2013_v1.lumicalc.sum'
 data4_lumifile = 'lumicalc/data_SingleMu_Run2012C_24Aug2012_v1.lumicalc.sum'
 data5_lumifile = 'lumicalc/data_SingleMu_Run2012C_PromptReco_v2.lumicalc.sum'
 data6_lumifile = 'lumicalc/data_SingleMu_Run2012D_PromptReco_v1.lumicalc.sum'
-wjets_lumifile = 'lumicalc/WplusJets_madgraph_Extension.lumicalc.sum'
+wjets_filtered_lumifile = 'lumicalc/WplusJets_madgraph_filtered.lumicalc.sum'
+wjets_extension_lumifile = 'lumicalc/WplusJets_madgraph_Extension.lumicalc.sum'
+wjets1_lumifile = 'lumicalc/Wplus1Jets_madgraph.lumicalc.sum'
+wjets2_lumifile = 'lumicalc/Wplus2Jets_madgraph.lumicalc.sum'
+wjets3_lumifile = 'lumicalc/Wplus3Jets_madgraph.lumicalc.sum'
+wjets4_lumifile = 'lumicalc/Wplus4Jets_madgraph.lumicalc.sum'
 zjets_lumifile = 'lumicalc/Zjets_M50.lumicalc.sum'
 tt_lumifile = 'lumicalc/TTplusJets_madgraph.lumicalc.sum'
 ww_lumifile = 'lumicalc/WWJetsTo2L2Nu_TuneZ2_8TeV.lumicalc.sum'
@@ -270,9 +323,27 @@ hgglfv_lumifile = 'lumicalc/LFV_GluGlu_H2Tau_M-126.lumicalc.sum'
 hvbfsm_lumifile = 'lumicalc/VBF_H2Tau_M-125.lumicalc.sum'
 
 
+
 #read lumicalc files
-f = open(wjets_lumifile).read().splitlines()
-wjets_efflumi = float(f[0])
+f = open(wjets_filtered_lumifile).read().splitlines()
+wjets_filtered_efflumi = float(f[0])
+
+f = open(wjets_extension_lumifile).read().splitlines()
+wjets_extension_efflumi = float(f[0])
+
+f = open(wjets1_lumifile).read().splitlines()
+wjets1_efflumi = float(f[0])
+
+f = open(wjets2_lumifile).read().splitlines()
+wjets2_efflumi = float(f[0])
+
+f = open(wjets3_lumifile).read().splitlines()
+print f
+wjets3_efflumi = float(f[0])
+
+f = open(wjets4_lumifile).read().splitlines()
+wjets4_efflumi = float(f[0])
+
 
 f = open(zjets_lumifile).read().splitlines()
 zjets_efflumi = float(f[0])
@@ -311,7 +382,10 @@ f = open(data6_lumifile).read().splitlines()
 data6_lumi = float(f[0])
 
 lumi = data1_lumi+data2_lumi+data3_lumi+data4_lumi+data5_lumi+data6_lumi
+#wjets_efflumi = wjets_filtered_efflumi+wjets1_efflumi+wjets2_efflumi+wjets3_efflumi+wjets4_efflumi
+wjets_efflumi = wjets_extension_efflumi
 #wjets_efflumi= 1580.01
+#wjets_efflumi = wjets_extension_efflumi+wjets1_efflumi+wjets2_efflumi+wjets3_efflumi+wjets4_efflumi
 #zjets_efflumi = 8505.626
 #tt_efflumi = 30529.576
 #ww_efflumi = 332328.452161#xsection = 5.82 pb
@@ -320,20 +394,37 @@ lumi = data1_lumi+data2_lumi+data3_lumi+data4_lumi+data5_lumi+data6_lumi
 #hvbflvf_efflumi = 6369426.75159
 #lumi = 18025.9 #inverse picobarns
 
+wjets_datanorm = lumi/wjets_efflumi
+zjets_datanorm = lumi/zjets_efflumi
+tt_datanorm = lumi/tt_efflumi
+ww_datanorm = lumi/ww_efflumi
+
+qcd_norm_histo = make_qcd_norm(presel, var, predir, savedir, channel ,wjets_pre_ntuple_file, zjets_pre_ntuple_file, ttbar_pre_ntuple_file, ww_pre_ntuple_file, data_pre_ntuple_file,data_ntuple_file, wjets_datanorm, zjets_datanorm, tt_datanorm, ww_datanorm)
+antiiso_ntuple_spot = "antiisomuon"+channel
+data_ntuple_file.cd(antiiso_ntuple_spot)
+qcd = ROOT.gDirectory.Get(var).Clone() #get antiisolated qcd shape
+
+qcd.Scale(qcd_norm_histo.Integral()/qcd.Integral()) #normalized qcd shape
 
 if shape_norm == False: #normalize histos to data
-	wjets_norm = lumi/wjets_efflumi
+	wjets_norm = wjets_datanorm
+	zjets_norm = zjets_datanorm
+	tt_norm = tt_datanorm
+	ww_norm = ww_datanorm
+	wjets1_norm = lumi/wjets1_efflumi
 	zjets_norm = lumi/zjets_efflumi
-	tt_norm = lumi/tt_efflumi
-	ww_norm = lumi/ww_efflumi
 	#smgg_higgs_norm = lumi/smgg_higgs_efflumi
 	hgglfv_norm = lumi/hgglfv_efflumi
 	hvbflfv_norm = lumi/hvbflfv_efflumi
 	hvbfsm_norm = lumi/hvbfsm_efflumi
-	hgglfv.Scale(5*hgglfv_norm)
-	hvbflfv.Scale(5*hvbflfv_norm)
-	hvbfsm.Scale(5*hvbfsm_norm)
+	hgglfv.Scale(hgglfv_norm)
+	hvbflfv.Scale(hvbflfv_norm)
+	hvbfsm.Scale(hvbfsm_norm)
+	wjets.Scale(wjets_norm)
+	#wjets1.Scale(wjets1_norm)
+	#wjets.Add(wjets1)
 else:   	#normailze histos to 1
+	#wjets.Add(wjets1)
 	wjets_norm = 1/wjets.Integral()
 	zjets_norm = 1/zjets.Integral()
 	tt_norm = 1/ttbar.Integral()
@@ -348,7 +439,6 @@ else:   	#normailze histos to 1
 	qcd.Scale(qcd_norm)
 
 zjets.Scale(zjets_norm)
-wjets.Scale(wjets_norm)
 ttbar.Scale(tt_norm)
 ww.Scale(ww_norm)
 
@@ -390,10 +480,10 @@ data.Rebin(binwidth)
 #legend.AddEntry(signal_mc_vbf),'VBF LFV MC')
 if shape_norm == False:
 	if "gg" in channel:
-		legend.AddEntry(hgglfv,'5 X GG LFV Higgs')
+		legend.AddEntry(hgglfv,'GG LFV Higgs')
 	if "vbf" in channel:
-		legend.AddEntry(hvbflfv, '5 X VBF LFV Higgs')
-		legend.AddEntry(hvbfsm, '5 X VBF SM Higgs')
+		legend.AddEntry(hvbflfv, 'VBF LFV Higgs')
+		legend.AddEntry(hvbfsm, 'VBF SM Higgs')
 
 else:
         if "gg" in channel:
@@ -424,6 +514,32 @@ if "gg" in channel:
 if "vbf" in channel:
 	hvbflfv.Draw('sameshist')
 	hvbfsm.Draw('sameshist')
+
+maxMC = LFVStack.GetMaximum()
+maxData = data.GetMaximum()
+if maxData>maxMC:
+	maxHist = maxData
+else:
+	maxHist = maxMC
+LFVStack.SetMaximum(maxHist*1.05)
+
+if presel == True:
+	data.Draw("sames")
+elif blind == True:
+	binblindlow = data.FindBin(blindlow)
+	binblindhigh = data.FindBin(blindhigh)
+	for x in range(binblindlow, binblindhigh):
+		data.SetBinContent(x, -1000)
+		
+	data.Draw('sames')
+	legend.AddEntry(data,'8 TeV Data')	
+	pave = ROOT.TPave(blindlow,0,blindhigh,maxHist*1.1,4,"br")
+	pave.SetFillColor(1)
+	pave.SetFillStyle(3002)
+	#pave.SetDrawOption(0)
+	pave.SetBorderSize(0)
+	pave.Draw()
+	
 legend.Draw('sames')
 LFVStack.GetXaxis().SetTitle(xlabel)
 LFVStack.GetYaxis().SetTitleOffset(1.2)
