00a7af3555aec7d58084abe6bb767d3aec49e9c2
Mauro Verzetti <mverzett@cern.ch>
2013-07-10 04:16:50 -0500

    updated 7TeV part as 8 TeV

--- wh/setup.sh
+++ wh/setup.sh
@@ -3,14 +3,20 @@
 # Setup the cython proxies, find input ntuple files, and compute luminosity.
 
 source jobid.sh
-#export jobid=$jobid7
-#export datasrc=$(ls -d /scratch/*/data/$jobid | awk -F$jobid '{print $1}')
-#
-#for dir in $datasrc; do
-#    rake "meta:getinputs[$jobid, $datasrc]"
-#    rake "meta:getmeta[inputs/$jobid, mm/metaInfo, 7]"
-#done
-#
+export jobid=$jobid7
+export datasrc=$(ls -d /scratch/*/data/$jobid | awk -F$jobid '{print $1}')
+
+for dir in $datasrc; do
+    echo $dir
+    rake "meta:getinputs[$jobid, $dir, mm/metaInfo]"
+    rake "meta:getmeta[inputs/$jobid, mm/metaInfo, 7]"
+done
+
+rm inputs/$jobid/WZJetsTo3LNu_ZToTauTau*
+for file in $(ls inputs/$jobid/WZJetsTo3LNu*); do
+    newname=`echo $file | sed 's|WZJetsTo3LNu|WZJetsTo3LNu_ZToTauTau|'`
+    cp -uv $file $newname
+done
 
 export jobid=$jobid8
 export datasrc=$(ls -d /scratch/*/data/$jobid | awk -F$jobid '{print $1}')
