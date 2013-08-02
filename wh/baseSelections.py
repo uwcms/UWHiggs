7ae3f0aba6e1132fc5bb9f1297433f74109850b2
Mauro Verzetti <mverzett@cern.ch>
2013-08-02 05:48:57 -0500

    switch to JetCSVBtag in lepton definition

--- wh/baseSelections.py
+++ wh/baseSelections.py
@@ -15,7 +15,8 @@ def muSelection(row, name):
     if getattr( row, getVar(name,'Pt')) < 10:       return False
     if getattr( row, getVar(name,'AbsEta')) > 2.4:  return False
     if not getattr( row, getVar(name,'PixHits')):   return False
-    if getattr( row, getVar(name,'JetBtag')) > 3.3: return False
+    if getattr( row, getVar(name,'JetCSVBtag')) > 0.8: return False
+    #if getattr( row, getVar(name,'JetBtag')) > 3.3: return False #was 3.3 
     if abs(getattr( row, getVar(name,'DZ'))) > 0.2: return False
     return True
 
@@ -25,7 +26,8 @@ def eSelection(row, name):
     if getattr( row, getVar(name,'MissingHits')):       return False
     if getattr( row, getVar(name,'HasConversion')):     return False
     if not getattr( row, getVar(name,'ChargeIdTight')): return False
-    if getattr( row, getVar(name,'JetBtag')) > 3.3:     return False
+    if getattr( row, getVar(name,'JetCSVBtag')) > 0.8: return False
+    #if getattr( row, getVar(name,'JetBtag')) > 3.3:     return False
     if abs(getattr( row, getVar(name,'DZ'))) > 0.2:     return False
     return True
     
