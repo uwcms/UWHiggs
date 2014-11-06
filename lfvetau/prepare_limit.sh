da60e3aa8534e054a3111ae82b397cdc7d46c01c
Mauro Verzetti <mverzett@cern.ch>
2014-11-06 11:27:33 -0600

    First version. Prepares the datacards for limit setting, significance computation etc.

---
+++ lfvetau/prepare_limit.sh
@@ -0,0 +1,37 @@
+#! /bin/env bash
+
+#$1 --> gategoy (0, 1, 2) --> 0 jets, 1 jet, vbf
+category=$1
+input_dir=plots/$jobid/lfvet/
+output_dir=limits/$jobid/$category/
+mkdir -p output_dir
+
+#copy basic config
+cp card_config/cgs.$category.conf $output_dir/cgs.config
+cp card_config/unc.$category.conf $output_dir/unc.config
+cp card_config/unc.$category.vals $output_dir/unc.vals
+
+#copy shape file
+cp $input_dir/shapes.$category.root $output_dir/shapes.root
+
+#add dynamically assigned variables
+cat $input_dir/unc.$category.conf >> $output_dir/unc.config
+cat $input_dir/unc.$category.vals >> $output_dir/unc.vals
+
+#add bbb errors 
+if [ $category == '0' ]; then
+    command='gg0etau:fakes'
+elif [ $category == '1' ]; then
+    command='boostetau:fakes'    
+else
+    command='vbfetau:fakes,ttbar,singlet,ztautau,SMVBF126,WWVBF126'
+fi
+
+#TODO: normalize or not?
+#will it work with input and output in the same place?
+add_bbb_errors.py --normalize -i $output_dir -o $output_dir $command
+
+#build datacard
+pushd $output_dir
+create-datacard.py -i shapes.root
+popd
