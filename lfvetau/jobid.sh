fd9877e08ddac960d40a09ff2a026dc4c7791fad
Mauro Verzetti <mverzett@cern.ch>
2014-11-12 00:02:53 -0600

    update jobid

--- lfvetau/jobid.sh
+++ lfvetau/jobid.sh
@@ -6,5 +6,5 @@
 #export jobid8='newNtuple_2Sept'
 #export jobid8='newNtuple_1Oct'
 export jobidmt='MCntuples_8April'
-export jobid8='newNtuple_16Oct'
+export jobid8='newNtuple_5Nov'
 export jobidSync='newNtuple_Sync'
