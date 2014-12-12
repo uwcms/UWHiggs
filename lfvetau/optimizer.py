b59b2d24be926253548a34a2b5ea19c5507b6672
Mauro Verzetti <mverzett@cern.ch>
2014-12-12 02:57:13 -0600

    optimizer

--- lfvetau/optimizer.py
+++ lfvetau/optimizer.py
@@ -4,59 +4,65 @@
 import os
 import itertools
 
-def make_0j_selector(tthr, ethr, e_t_DPhi, tMT):
-    def selector(row):
-        return row.tPt > tthr and \
-             row.ePt > ethr and \
-             row.e_t_DPhi > e_t_DPhi and \
-             row.tMtToPfMet < tMT
-    return selector
-
-def make_1j_selector(tthr, ethr, tMT):
-    def selector(row):
-        return row.tPt > tthr and \
-            row.ePt > ethr and \
-            row.tMtToPfMet < tMT
-    return selector
-
-def make_2j_selector(tthr, ethr, tMT, vbfmass, vbfdeta):
-    def selector(row):
-        return row.tPt > tthr and \
-            row.ePt > ethr and \
-            row.tMtToPfMet < tMT and \
-            row.vbfMass > vbfmass and \
-            row.vbfDeta > vbfdeta
-    return selector
-
 RUN_OPTIMIZATION = ('RUN_OPTIMIZATION' in os.environ) and eval(os.environ['RUN_OPTIMIZATION'])
 
-grid_search = {
-    0 : {},
-    1 : {},
-    2 : {},
+_0jets = {
+    'tPt'  : range(20,70,10)+[35,45],
+    'ePt'  : range(20,70,10)+[35,45],
+    'dphi' : [3.14, 3.00, 2.7, 2.4, 2.2],
+    'tMtToPfMet' : range(0,50,10)+[35],
 }
+_0jet_region_template = 'tPt%i_ePt%i_dphi%.2f_tMtToPfMet%i'
+def _get_0jet_regions(tPt, ePt, dphi, tMtToPfMet):
+    pass_tPt        = [i for i in _0jets['tPt'       ] if tPt        > i] 
+    pass_ePt        = [i for i in _0jets['ePt'       ] if ePt        > i] 
+    pass_dphi       = [i for i in _0jets['dphi'      ] if dphi       > i] 
+    pass_tMtToPfMet = [i for i in _0jets['tMtToPfMet'] if tMtToPfMet < i] 
+    return [_0jet_region_template % i for i in itertools(pass_tPt, pass_ePt, pass_dphi, pass_tMtToPfMet)]
 
+
+_1jets = {
+    'tPt'  : range(20,70,10)+[35,45],
+    'ePt'  : range(20,70,10)+[35,45],
+    'tMtToPfMet' : range(0,50,10)+[35],
+}
+_1jet_region_template = 'tPt%i_ePt%i_tMtToPfMet%i'
+def _get_1jet_regions(tPt, ePt, tMtToPfMet):
+    pass_tPt        = [i for i in _1jets['tPt'       ] if tPt        > i]
+    pass_ePt        = [i for i in _1jets['ePt'       ] if ePt        > i]
+    pass_tMtToPfMet = [i for i in _1jets['tMtToPfMet'] if tMtToPfMet < i]
+    return [_1jet_region_template % i for i in itertools(pass_tPt, pass_ePt, pass_tMtToPfMet)]
+
+_2jets = {
+    'tPt'  : range(20,70,10)+[35,45],
+    'ePt'  : range(20,70,10)+[35,45],
+    'tMtToPfMet' : range(0,50,10)+[35],
+    'vbf_mass' : range(400, 700, 100) + [550],
+    'vbf_deta' : [3.0, 3.5, 4.0],
+}
+_2jet_region_template = 'tPt%i_ePt%i_tMtToPfMet%i_vbf_mass%i_vbf_deta%.1f'
+def _get_2jet_regions(tPt, ePt, dphi, tMtToPfMet, vbf_mass, vbf_deta):
+    pass_tPt        = [i for i in _2jets['tPt'       ] if tPt        > i]
+    pass_ePt        = [i for i in _2jets['ePt'       ] if ePt        > i]
+    pass_tMtToPfMet = [i for i in _2jets['tMtToPfMet'] if tMtToPfMet < i]
+    pass_vbf_mass = [i for i in _2jets['vbf_mass'] if vbf_mass < i]
+    pass_vbf_deta = [i for i in _2jets['vbf_deta'] if vbf_deta < i]
+    return [_2jet_region_template % i for i in itertools(pass_tPt, pass_ePt, pass_tMtToPfMet, pass_vbf_mass, pass_vbf_deta)]
+
+def empty(*args):
+    return []
+
+compute_regions_0jet = _get_0jet_regions if RUN_OPTIMIZATION else empty
+compute_regions_1jet = _get_1jet_regions if RUN_OPTIMIZATION else empty
+compute_regions_2jet = _get_2jet_regions if RUN_OPTIMIZATION else empty
+
+regions = {0 : [], 1 : [], 2 : []}
 if RUN_OPTIMIZATION:
-    t_thrs = range(20,70,10)+[35,45]
-    e_thr  = range(20,70,10)+[35,45]
-
-    mt_thr = range(0,50,10)+[35]
-    dphi = [3.14, 3.00, 2.7, 2.4, 2.2]
-    vbf_mass = range(400, 700, 100) + [550]
-    vbf_deta = [3.0, 3.5, 4.0]
-    
-    gg_template = 't%i_e%i_dp%.1f_mt%i'
-    for thresholds in itertools.product(t_thrs, e_thr, dphi, mt_thr):
-        grid_search[0][gg_template % thresholds] = make_0j_selector(*thresholds)
-
-    boost_template = 't%i_e%i_mt%i'
-    for thresholds in itertools.product(t_thrs, e_thr, mt_thr):
-        grid_search[1][boost_template % thresholds] = make_1j_selector(*thresholds)
-
-    vbf_template = 't%i_e%i_mt%i_vbfm%i_vbfeta%i'
-    for thresholds in itertools.product(t_thrs, e_thr, mt_thr, vbf_mass, vbf_deta):
-        grid_search[2][vbf_template % thresholds] = make_2j_selector(*thresholds)
-
+    regions = {
+        0 : [_0jet_region_template % i for i in itertools(_0jets['tPt'], _0jets['ePt'], _0jets['dphi'], _0jets['tMtToPfMet'])],
+        1 : [_1jet_region_template % i for i in itertools(_1jets['tPt'], _1jets['ePt'], _1jets['tMtToPfMet'])],
+        2 : [_2jet_region_template % i for i in itertools(_2jets['tPt'], _2jets['ePt'], _2jets['tMtToPfMet'], _2jets['vbf_mass'], _2jets['vbf_deta'])],
+        }
 
 if __name__ == "__main__":
     from pdb import set_trace
